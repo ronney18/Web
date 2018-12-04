8a24f0a3a06aa4047b496f85b72ec61d867ae610
ronney18 <44005685+ronney18@users.noreply.github.com>
2018-12-03 22:24:53 -0500

    Update FormValidator.js

--- hw7/js/FormValidator.js
+++ hw7/js/FormValidator.js
@@ -1,21 +1,21 @@
 /*
 File: http://ronney18.github.io/Web/hw7/MultiplicationTable.html
-	COMP4610 Assignment 7: Creating a Multiplication Table with JQuery Validation Plugin
-	Ronney Sanchez, UMass Lowell Computer Science, Ronney_Sanchez@student.uml.edu
-	Copyright (c) 2018 by Ronney Sanchez.
-	updated by Ronney Sanchez on November 24, 2018 at 11:30 PM
+COMP4610 Assignment 7: Creating a Multiplication Table with JQuery Validation Plugin
+Ronney Sanchez, UMass Lowell Computer Science, Ronney_Sanchez@student.uml.edu
+Copyright (c) 2018 by Ronney Sanchez.
+updated by Ronney Sanchez on November 24, 2018 at 11:30 PM
 
-	Sources: https://www.w3schools.com/js/default.asp
-		 https://jqueryvalidation.org/
-						Albara Mehene (class mate)
-						Marittya Keu (class mate)
+Sources: https://www.w3schools.com/js/default.asp
+         https://jqueryvalidation.org/
+          Albara Mehene (class mate)
+          Marittya Keu (class mate)
 
-	Description: This web page prompts the user for a specific number of rows and
-	columns and displays a multiplication table starting at the specified row
-	and column number and ending at a certain row and column number. This web
-	also validates for user inputs as soon as the user enters an input. If the input
-	is invalid, the user will automatically get notified by the JQuery validation
-	plugin.
+Description: This web page prompts the user for a specific number of rows and
+columns and displays a multiplication table starting at the specified row
+and column number and ending at a certain row and column number. This web
+also validates for user inputs as soon as the user enters an input. If the input
+is invalid, the user will automatically get notified by the JQuery validation
+plugin.
 */
 //Validating the form from the html page
 $(document).ready(function(){
@@ -25,45 +25,61 @@ $(document).ready(function(){
           //An input for the start of the column is required only using digits and no characters
             beginColumn:{
               required: true,
-              digits: true
+              number: true,
+              min: -150,
+              max: 150
             },
             //An input for the end of the column is required only using digits and no characters
             endColumn:{
               required: true,
-              digits: true
+              number: true,
+              min: -150,
+              max: 150
             },
             //An input for the start of the row is required only using digits and no characters
             beginRow:{
               required: true,
-              digits: true
+              number: true,
+              min: -150,
+              max: 150
             },
             //An input for the end of the row is required only using digits and no characters
             endRow:{
               required: true,
-              digits: true
+              number: true,
+              min: -150,
+              max: 150
             }
         },
         //Display an error message before submitting if the user attempts to insert an ivalid input
         messages:{
             //Displaying an error message for the start of the column with requirements and digit validation
             beginColumn:{
-              required: "Please enter a number from 1 to 350",
-              digits: "Only non-zero positive numbers are allowed!"
+              required: "Please enter a number from -150 to 150 except 0",
+              number: "Only numbers are allowed!",
+              min: "The minimum is -150!",
+              max: "The maximum is 150!"
             },
             //Displaying an error message for the end of the column with requirements and digit validation
             endColumn:{
-              required: "Please enter a number from 1 to 350",
-              digits: "Only non-zero positive numbers are allowed!"
+              required: "Please enter a number from -150 to 150 except 0",
+              number: "Only numbers are allowed!",
+              min: "The minimum is -150!",
+              max: "The maximum is 150!"
             },
             //Displaying an error message for the start of the row with requirements and digit validation
             beginRow:{
-              required: "Please enter a number from 1 to 350",
-              digits: "Only non-zero positive numbers are allowed!"
+              required: "Please enter a number from -150 to 150 except 0",
+              number: "Only numbers are allowed!",
+              min: "The minimum is -150!",
+              max: "The maximum is 150!"
             },
             //Displaying an error message for the end of the row with requirements and digit validation
             endRow:{
-              required: "Please enter a number from 1 to 350",
-              digits: "Only non-zero positive numbers are allowed!"
+              required: "Please enter a number from -150 to 150 except 0",
+              number: "Only numbers are allowed!",
+              min: "The minimum is -150!",
+              max: "The maximum is 150!"
             }
         }
     });
