42ed76d506d3be7affe4be78927aea01a43b9bd8
ronney18 <44005685+ronney18@users.noreply.github.com>
2018-12-06 17:04:52 -0500

    Update FormValidator.js

--- hw7/js/FormValidator.js
+++ hw7/js/FormValidator.js
@@ -18,28 +18,8 @@ is invalid, the user will automatically get notified by the JQuery validation
 plugin.
 */
 
-//The document will run these function whenever is ready
-$(document).ready(function() {
-    validate();
-    valid();
-});
-
-//Checking if the form is valid in order to submit
-function valid(){
-    //Submit the form if the form is valid
-    if($("#myForm").valid())
-    {
-        $("#myForm").submit();
-    }
-    //Don't do anything else of there is an error on the form
-    else
-    {
-        return;
-    }
-}
-
 //Validating the form from the html page
-function validate{
+$(document).ready(function(){
     $("#myForm").validate({
         //Setting the rules of every rows and columns of the table of the form
         rules:{
@@ -103,100 +83,5 @@ function validate{
               max: "The maximum is 150!"
             }
         },
-      //Make the table if there is no error or empty slots in the form
-      submitHandler: function(event){
-          makeTable();
-          //Return false to the user
-          return false;
-      },
-      //Hide the table if there is an invalid submission on the form
-      invalidHandler: function(){
-          $("#div1").empty();
-      }
     });
-}
-
-function makeTable()
-{
-  //Getting the values of the rows and columns from the html page
-  var StartColumn = Number(document.getElementById('beginColumn').value);
-  var EndColumn = Number(document.getElementById('endColumn').value);
-  var StartRow = Number(document.getElementById('beginRow').value);
-  var EndRow = Number(document.getElementById('endRow').value);
-  var temp;
-  //Swap the start of the row with the end of the row if the end of the row is smaller
-  if(EndRow < StartRow){
-    //Assign the start of the row to a temporary variable
-    temp = StartRow;
-    //Assign the end of the row to the start of the row
-    StartRow = EndRow;
-    //Assign the temporary value to the end of the row
-    EndRow = temp;
-  }
-
-  //Swap the start of the column with the end of the column if the end of the column is smaller
-  if(EndColumn < StartColumn){
-    //Assign the start of the column to a temporary variable
-    temp = StartColumn;
-    //Assign the end of the column to the start of the column
-    StartColumn = EndColumn;
-    //Assign the temporary value to the end of the column
-    EndColumn = temp;
-  }
-
-  //Creating a table using the id of the divider in the HTML page
-  //Albara Mehene (class mate) helped me in doing this table
-  const table = document.getElementById("div1");
-  //Creating the elements and the body of the table
-  const tableEl = document.createElement("table");
-  const tableBody = document.createElement("tbody");
-
-  //Creating a table row
-  var rows = document.createElement("tr");
-  //Append the table header to the table row
-  rows.appendChild(document.createElement("th"));
-  //Looping through the rows of the table
-  for(let i = StartRow; i <= EndRow; i++){
-      //Creating an element for the table header
-      const header = document.createElement("th");
-      //Assign the indices to the header
-      header.innerText = i;
-      //Append the header to the table rows
-      rows.appendChild(header);
-  }
-  //Append the rows to the table body
-  tableBody.appendChild(rows);
-
-  //Looping through the columns of the table
-  for(let j = StartColumn; j <= EndColumn; j++){
-      //Creating an element for the table rows
-      rows = document.createElement("tr");
-      //Creating an element for the table header
-      let header = document.createElement("th");
-      //Creating a node for the indices
-      let node = document.createTextNode(j);
-      //Append the node to the header
-      header.appendChild(node);
-      //Append the header to the table rows
-      rows.appendChild(header);
-      //Looping through the rows of the table
-      for(let k = StartRow; k <= EndRow; k++){
-          //Creating the cells for the table
-          var blocks = document.createElement("td");
-          //Creating the node by multiplying the indices of the rows and colums
-          var blockNode = document.createTextNode(j * k);
-          //Append the node to the cells of the table
-          blocks.appendChild(blockNode);
-          //Append the cells to the rows of the table
-          rows.appendChild(blocks);
-      }
-      //Append the rows to the table body
-      tableBody.appendChild(rows);
-  }
-  //Append the body to the elements of the table
-  tableEl.appendChild(tableBody);
-  //Append the elements to the table
-  table.appendChild(tableEl);
-  //Replace the elements with the node at the starting point
-  table.replaceChild(tableEl, table.childNodes[0]);
-}
+});
